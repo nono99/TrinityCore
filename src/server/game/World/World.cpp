947245d5d6651ffedb5c8f914f0a31c083404410
Fredi Machado <fredisoft@gmail.com>
2011-05-15 21:34:17 -0300

    AHBot patch

--- src/server/game/World/World.cpp
+++ src/server/game/World/World.cpp
@@ -75,6 +75,7 @@
 #include "SmartAI.h"
 #include "Channel.h"
 #include "AnticheatMgr.h"
+#include "AuctionHouseBot.h"
 
 volatile bool World::m_stopEvent = false;
 uint8 World::m_ExitCode = SHUTDOWN_EXIT_CODE;
@@ -1724,6 +1725,9 @@ void World::SetInitialWorldSettings()
     sLog->outString("Calculate random battleground reset time..." );
     InitRandomBGResetTime();
 
+    sLog->outString("Initialize AuctionHouseBot...");
+    auctionbot.Initialize();
+
     // possibly enable db logging; avoid massive startup spam by doing it here.
     if (sLog->GetLogDBLater())
     {
@@ -1890,6 +1894,7 @@ void World::Update(uint32 diff)
     /// <ul><li> Handle auctions when the timer has passed
     if (m_timers[WUPDATE_AUCTIONS].Passed())
     {
+        auctionbot.Update();
         m_timers[WUPDATE_AUCTIONS].Reset();
 
         ///- Update mails (return old mails with item, or delete them)
