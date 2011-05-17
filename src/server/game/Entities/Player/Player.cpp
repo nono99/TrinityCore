61a5218ab89e00a38778401c762314dd34e4d402
Fredi Machado <fredisoft@gmail.com>
2011-05-17 17:12:32 -0300

    Revert "Fixed a bug with disabling PvP state at hostile zones."
    This reverts commit caea052268f54eb6390762672a1f327bf0c74c54.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -19075,8 +19075,6 @@ void Player::UpdatePvPFlag(time_t currTime)
         return;
     if (pvpInfo.endTimer == 0 || currTime < (pvpInfo.endTimer + 300) || pvpInfo.inHostileArea)
         return;
-    if (pvpInfo.inHostileArea)
-        return;
 
     UpdatePvP(false);
 }
